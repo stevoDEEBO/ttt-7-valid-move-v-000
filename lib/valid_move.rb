cd34615bd9d99aec5e9d4486968035eee0efd1ea
Stephen Telesmanic <steve.telesmanic@gmail.com>
2018-02-24 04:14:08 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -14,7 +14,7 @@ end
 def position_taken?(board, index)
   if board[index] == " " #|| board[index] == "" || board[index] == nil
     false
-  elsif board[index] == "X" || board[index] == "O"
+  else #if board[index] == "X" || board[index] == "O"
     true
   end
 end
